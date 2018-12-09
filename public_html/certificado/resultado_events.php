b9acdb4532cb7a58faa42b22bd592d6a00034bac
Adrian Garcia <adriang_1174@hotmail.com>
2018-12-09 12:58:10 -0300

    Results in PDF

--- public_html/certificado/resultado_events.php
+++ public_html/certificado/resultado_events.php
@@ -28,30 +28,36 @@ function certificado_txt_archivo_BeforeShow(& $sender)
 	        $str = file_get_contents($path.$archivo);
 	}
 
-	$str = StrTr($str,"\x21", " ");
-	$str = Str_replace("\x21", "",$str);
-	$str = Str_replace("\x10", "",$str);
-	$str = Str_replace("\x1B", "",$str);
-	$str = Str_replace("\x08", "",$str);
-	$str = Str_replace("\x00", "",$str);
-//
-	$str = StrTr($str,"\x10", "");
-	$str = StrTr($str,"\x1B", " ");
-	$str = StrTr($str,"\x08", " ");
-	$str = StrTr($str,"\x00", " ");
-	$str = StrTr($str,"\x0A", " ");
-//
-	$str = StrTr($str,"\x82", "é");
-	$str = StrTr($str,"\xA2", "ó");
-	$str = StrTr($str,"\xA4", "ñ");
-	$str = Str_replace("\x12", "",$str);
-	$str = Str_replace("(s3B", "",$str);
-	$str = Str_replace("(s11H", "",$str);
-	$str = Str_replace("(s0S", "",$str);
-	$str = Str_replace("(s0B", "",$str);
-	$str = Str_replace("(s7B", "",$str);
-	$str = StrTr($str,"\x0D", chr(10));
-	
+	if (substr($archivo, -3) <> 'pdf'){
+
+		$str = StrTr($str,"\x21", " ");
+		$str = Str_replace("\x21", "",$str);
+		$str = Str_replace("\x10", "",$str);
+		$str = Str_replace("\x1B", "",$str);
+		$str = Str_replace("\x08", "",$str);
+		$str = Str_replace("\x00", "",$str);
+	//
+		$str = StrTr($str,"\x10", "");
+		$str = StrTr($str,"\x1B", " ");
+		$str = StrTr($str,"\x08", " ");
+		$str = StrTr($str,"\x00", " ");
+		$str = StrTr($str,"\x0A", " ");
+	//
+		$str = StrTr($str,"\x82", "é");
+		$str = StrTr($str,"\xA2", "ó");
+		$str = StrTr($str,"\xA4", "ñ");
+		$str = Str_replace("\x12", "",$str);
+		$str = Str_replace("(s3B", "",$str);
+		$str = Str_replace("(s11H", "",$str);
+		$str = Str_replace("(s0S", "",$str);
+		$str = Str_replace("(s0B", "",$str);
+		$str = Str_replace("(s7B", "",$str);
+		$str = StrTr($str,"\x0D", chr(10));
+	}
+	else{
+		$str = "<a href=../archivos/".$archivo."> Ver resultados </a> ";
+	}
+
 $certificado->txt_archivo->SetValue($str);
 // -------------------------
 //End Custom Code
